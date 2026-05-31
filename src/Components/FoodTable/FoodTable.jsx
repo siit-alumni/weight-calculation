155dd71bde16ae927f5ab8600a95eca7c3758355
Gherasim Rares <rares.gherasim@gmail.com>
2026-05-31 10:38:31 +0300

    #38 Loaded the foods fromfoodDB.json and displayed them in the component

--- src/Components/FoodTable/FoodTable.jsx
+++ src/Components/FoodTable/FoodTable.jsx
@@ -4,6 +4,7 @@ import { use, useContext, useEffect, useState } from "react";
 import { UserContext } from "../../App";
 import { getUserDataFromLocalStorage, getUsersFromLocalStorage, saveUserDataToLocalStorage, sortUsersAlphabetically } from "../functions/functions";
 import { useTranslation } from "react-i18next";
+import foodData from '../../assets/foodDB.json';
 
 export default function FoodTable() {
     const { t } = useTranslation();
@@ -16,6 +17,31 @@ export default function FoodTable() {
         <div>
             <h2>{t("foodTable.title")}</h2>
             <p>{t("foodTable.description")}</p>
+
+            <table className="table table-bordered table-hover">
+                <thead>
+                    <tr>
+                        <th>{t("foodTable.product")}</th>
+                        <th>{t("common.Macronutrients.calories")}/100g</th>
+                        <th>{t("common.Macronutrients.protein")}</th>
+                        <th>{t("common.Macronutrients.carbs")}</th>
+                        <th>{t("common.Macronutrients.fat")}</th>
+                        <th>{t("common.Macronutrients.fiber")}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {Object.entries(foodData).map(([foodName, foodInfo]) => (
+                        <tr key={foodName}>
+                            <td>{foodName}</td>
+                            <td>{foodInfo["Calories 100g"]}</td>
+                            <td>{foodInfo["Protein g"]}</td>
+                            <td>{foodInfo["Carbs g"]}</td>
+                            <td>{foodInfo["Fat g"]}</td>
+                            <td>{foodInfo["Fiber g"]}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
             <button onClick={handleUserSelection} className="btn btn-secondary col-md-4 mt-3">{t("report.userSelectionButton")}</button>
         </div>
 
