f54c10e089da163f704ca5652b628c0ba473c934
Ovidiu <eu_oni@yahoo.com>
2026-06-08 22:39:14 +0300

    Added modal for delete,edit,info

--- src/Components/UsersList/UsersList.jsx
+++ src/Components/UsersList/UsersList.jsx
@@ -6,6 +6,11 @@ import './UsersList.css';
 import { UserContext } from '../../App';
 import { useNavigate } from 'react-router-dom';
 import UserData from '../UserData/UserData';
+import SelectUser from '../SelectUser/SelectUser';
+import { ModifyUser } from '../ModifyUser/ModifyUser';
+import { DeleteUser } from '../DeleteUser/DeleteUser';
+import UserListSort from './UserListSort';
+import { Results } from '../Results/Results';
 
 export default function UsersList({ users }) {
 
@@ -14,6 +19,9 @@ export default function UsersList({ users }) {
     const [selectedUserId, setSelectedUserId] = useState(getUserDataFromLocalStorage());
     const { t } = useTranslation();
     const navigate = useNavigate();
+   
+    const [modalType, setModalType] = useState("");
+
 
     const displayUserDataText = displayUser ? t("selectUser.hideUserDataButton") : t("selectUser.displayUserButton");
     const handleListSelectUser = (userId) => {
@@ -27,21 +35,29 @@ export default function UsersList({ users }) {
         setUserData(userData);
         saveUserDataToLocalStorage(userData);
         navigate('/results');
+        // setModalType("Select");
     };
 
     const handleUserSelection = () => {
-        navigate('/selectUser');
+        // navigate('/selectUser');
+        // setUserData(userData);
+        // setModalType("Select");
     }
-
-    const handleUpdateUser = () => {
-        navigate('/editUser');
-    };
-
+    //eu
     const handleDeleteUser = () => {
-        navigate('/deleteUser');
+        setUserData(userData);
+        setModalType("Delete");
     };
+    //eu
+    const handleUpdateUser = () => {
+        setUserData(userData);
+        setModalType("Edit");
+    };
+
     const handleDisplayUser = () => {
         // setDisplayUser(prev => !prev);
+        setUserData(userData);
+        setModalType("Display");
     }
 
 
@@ -65,19 +81,19 @@ export default function UsersList({ users }) {
                                 <button className="btn btn-primary  " onClick={handleUpdateUser}>{t("selectUser.modifyButton")}</button>
                                 <button className="btn btn-primary " onClick={handleDeleteUser}>{t("selectUser.deleteButton")}</button>
                                 <button className="btn btn-primary {displayUser ? 'active' : ''}" aria-pressed={displayUser} onClick={handleDisplayUser}>{displayUserDataText}</button> */}
-                                <a class="icon-link" title={t("usersList.selectUserIcon")} onClick={handleSelectUser}>
+                                <a class="icon-link" title={t("usersList.selectUserIcon")} onClick={handleSelectUser} >
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2-square" viewBox="0 0 16 16">
                                         <path d="M3 14.5A1.5 1.5 0 0 1 1.5 13V3A1.5 1.5 0 0 1 3 1.5h8a.5.5 0 0 1 0 1H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8a.5.5 0 0 1 1 0v5a1.5 1.5 0 0 1-1.5 1.5z" />
                                         <path d="m8.354 10.354 7-7a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0" />
                                     </svg>
                                 </a>
-                                <a class="icon-link" title={t("usersList.deleteUserIcon")} onClick={handleDeleteUser}>
+                                <a class="icon-link" title={t("usersList.deleteUserIcon")} onClick={handleDeleteUser} data-bs-toggle="modal" data-bs-target="#userInfoModal">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                                         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
                                         <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
                                     </svg>
                                 </a>
-                                <a class="icon-link" title={t("usersList.updateUserIcon")} onClick={handleUpdateUser}>
+                                <a class="icon-link" title={t("usersList.updateUserIcon")} onClick={handleUpdateUser} data-bs-toggle="modal" data-bs-target="#userInfoModal">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                                         <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
                                         <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
@@ -97,17 +113,23 @@ export default function UsersList({ users }) {
             </ul>
 
             {/* {displayUser && <UserData />} */}
-            
+
             <div class="modal fade" id="userInfoModal" tabindex="-1" aria-labelledby="userInfoModalLabel" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-content">
-                        {/* <div class="modal-header">
-                                <h1 class="modal-title fs-5" id="userInfoModalLabel">Modal title</h1>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div> */}
-                        <div class="modal-body">
-                            <UserData />
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="userInfoModalLabel">Modal title</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
+                        <div class="modal-body">
+                            {/* {modalType === "Select" && <Results />} */}
+                            {modalType === "Delete" && <DeleteUser />}
+                            {modalType === "Edit" && <ModifyUser />}
+                            {modalType === "Display" && <UserData />}
+
+                            {/* <ModifyUser /> */}
+                        </div>
+
                         <div class="modal-footer">
                             <button type="button" class="btn btn-primary" data-bs-dismiss="modal">{t("form.closeButton")}</button>
                         </div>
