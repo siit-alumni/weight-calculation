f2eb27f7ea5c77602535fd7e189a6c2f389db0a6
Gherasim Rares <rares.gherasim@gmail.com>
2026-07-26 09:50:51 +0300

    full functionality in test table

--- src/Components/TableTest/TableTest.jsx
+++ src/Components/TableTest/TableTest.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import DataTable from 'datatables.net-react';
 import DT from 'datatables.net-dt';
@@ -9,54 +9,58 @@ import { useTranslation } from 'react-i18next';
 export default function TableTest() {
     DataTable.use(DT);
 
-    const { t } = useTranslation();
+    const { t, i18n } = useTranslation();
     const navigate = useNavigate();
-    const foodList = foodData;
 
-        Object.entries(foodList).forEach(([foodName, foodInfo]) => {
-        foodList[foodName]["Name"] = t(`foodDB.product.${foodName}`);
-    });
+    const [foodTableData, setFoodTableData] = useState([]);
+    // const [columns, setColumns] = useState(columns2);
+    const columns2 = [
+        { title: t("foodTable.product"), data: 'Name' },
+        { title: t("foodDB.category.title"), data: 'Category' },
+        { title: t("foodDB.subgroup.title"), data: 'Subgroup' },
+        { title: t("foodDB.Calories 100g.title"), data: 'Calories 100g' },
+        { title: t("foodDB.Protein g.title"), data: 'Protein g' },
+        { title: t("foodDB.Carbs g.title"), data: 'Carbs g' },
+        { title: t("foodDB.Fat g.title"), data: 'Fat g' },
+        { title: t("foodDB.Fiber g.title"), data: 'Fiber g' },
+    ];
+    
+    const options = {
+        layout: {
+            topStart: 'info',
+            bottom: 'paging',
+            bottomStart: null,
+            bottomEnd: null
+        },
+        paging: true,
+        searching: true,
+        ordering: true,
+        info: true,
+        select: true,
+        responsive: true,
+    };
 
-        const foodTableData = Object.entries(foodList).map(([foodName, foodInfo]) => ({
-        ...foodInfo,
-        Subgroup: t(`foodDB.subgroup.${foodInfo.Subgroup}`),
-    }));
+    useEffect(() => {
+        const foodTableData = Object.entries(foodData).map(([foodName, foodInfo]) => ({
+            ...foodInfo,
+            Name: t(`foodDB.product.${foodName}`),
+            Subgroup: t(`foodDB.subgroup.${foodInfo.Subgroup}`),
+        }));
 
+        setFoodTableData(foodTableData);
+    }, [t]);
 
-console.log('foodData', foodData);
-console.log('foodList', foodList);
-console.log('foodTableData', foodTableData);
 
 
     return (
         <div>
             <h1>Table Test</h1>
             <DataTable
+                key={i18n.language}
                 id="example"
-                options={{
-                    layout: {
-            topStart: 'info',
-            bottom: 'paging',
-            bottomStart: null,
-            bottomEnd: null
-        },
-                    data: foodTableData,
-                    columns: [
-                        { title: t("foodTable.product"), data: 'Name',},
-                        { title: t("foodDB.category.title"), data: 'Category' },
-                        { title: t("foodDB.subgroup.title"), data: 'Subgroup' },
-                        { title: t("foodDB.Calories 100g.title"), data: 'Calories 100g' },
-                        { title: t("foodDB.Protein g.title"), data: 'Protein g' },
-                        { title: t("foodDB.Carbs g.title"), data: 'Carbs g' },
-                        { title: t("foodDB.Fat g.title"), data: 'Fat g' },
-                        { title: t("foodDB.Fiber g.title"), data: 'Fiber g' },
-                    ],
-                    paging: true,
-                    searching: true,
-                    ordering: true,
-                    info: true,
-                    select: true,
-                }}
+                data={foodTableData}
+                options={options}
+                columns={columns2}
             />
             <div style={{ display: 'flex', gap: 12 }}>
                 <button onClick={() => navigate('/usersList')}>{t("selectUser.userListButton")}</button>
