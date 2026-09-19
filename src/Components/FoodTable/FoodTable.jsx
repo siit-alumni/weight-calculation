fba608e38bbc2b7791365a059f75e59d88097f43
Gherasim Rares <rares.gherasim@gmail.com>
2026-09-20 00:56:05 +0300

    #39 Modified the table to be 10 elements per page and the posibility to change the page

--- src/Components/FoodTable/FoodTable.jsx
+++ src/Components/FoodTable/FoodTable.jsx
@@ -8,7 +8,6 @@ import foodData from '../../assets/foodDB.json';
 import SortIcon from "./SortIcon";
 
 export default function FoodTable({ foodList, onFilteredFoodChange, showFavourites }) {
-    // const foodList = foodData;
     const { t } = useTranslation();
     const navigate = useNavigate();
     const { userData, setUserData } = useContext(UserContext);
@@ -17,7 +16,10 @@ export default function FoodTable({ foodList, onFilteredFoodChange, showFavourit
     const [favoriteList, setFavoriteList] = useState(userFavorites);
     const [sortDirection, setSortDirection] = useState({ product: null, subgroup: null, calories: null, protein: null, carbs: null, fat: null, fiber: null });
     const sortDirectionRef = useRef(sortDirection);
+    const vectorList = Object.entries(foodList);
+    const [page, setPage] = useState(0);
 
+    console.log('vector de 5', vectorList[5]);
     useEffect(() => {
         setFavoriteList(userFavorites);
     }, [userData, selectedUser?.id]);
@@ -27,9 +29,7 @@ export default function FoodTable({ foodList, onFilteredFoodChange, showFavourit
     const handleUserSelection = () => {
         navigate('/selectUser');
     };
-    // const handleShowFavouritesChange = (event) => {
-    //     setShowFavourites(event.target.checked);
-    // };
+
     const handleProductSort = () => {
         const direction = sortDirectionRef.current.product === 'asc' ? 'desc' : 'asc';
         sortDirectionRef.current = {
@@ -81,7 +81,7 @@ export default function FoodTable({ foodList, onFilteredFoodChange, showFavourit
                 (direction === 'asc' ? 1 : -1) *
                 foodA["Protein g"] - foodB["Protein g"]
             ));
-                    setSortDirection({
+        setSortDirection({
             product: null,
             subgroup: null,
             calories: null,
@@ -178,7 +178,7 @@ export default function FoodTable({ foodList, onFilteredFoodChange, showFavourit
             onFilteredFoodChange(sortedFoodList);
         }
     };
-    console.log('foodData', foodData);
+    console.log('foodList', foodList);
 
 
     return (
@@ -247,7 +247,7 @@ export default function FoodTable({ foodList, onFilteredFoodChange, showFavourit
                     </tr>
                 </thead>
                 <tbody>
-                    {Object.entries(foodList).map(([foodName, foodInfo]) => {
+                    {Object.entries(foodList).slice(page * 10, page * 10 + 10).map(([foodName, foodInfo]) => {
                         const isFavourite = favoriteList.includes(foodName);
 
                         if (showFavourites && !isFavourite) {
@@ -273,8 +273,8 @@ export default function FoodTable({ foodList, onFilteredFoodChange, showFavourit
                                                 updateUserInLocalStorage(updatedUser);
                                                 setUserData(selectedUser.id);
                                             }}
-                                            aria-label={isFavourite ? "Remove favorite" : "Add favorite"}
-                                            title={isFavourite ? "Remove favorite" : "Add favorite"}
+                                            aria-label={isFavourite ? t("foodTable.removeFromFavourites") : t("foodTable.addToFavourites")}
+                                            title={isFavourite ? t("foodTable.removeFromFavourites") : t("foodTable.addToFavourites")}
                                         >
                                             <svg
                                                 viewBox="0 0 24 24"
@@ -302,6 +302,64 @@ export default function FoodTable({ foodList, onFilteredFoodChange, showFavourit
                     })}
 
                 </tbody>
+                <tfoot>
+                    <tr>
+                        <td colSpan="8" >
+                            <div className="d-flex flex-wrap align-items-center align-content-center justify-content-between">
+
+                                <div>
+                                    <p style={{ marginBottom: '0px', paddingLeft: '2rem' }}>{t("foodTable.page")} {page + 1} {t("foodTable.of")} {Math.ceil(vectorList.length / 10)}</p>
+                                </div>
+                                <div style={{  paddingRight: '2rem' }}>
+                                    <button
+                                        type="button"
+                                        className="btn btn-link p-0"
+                                        style={{ paddingBottom: "100px", color: page == 0 ? "grey" : "black" }}
+                                        onClick={() => setPage(0)} disabled={page === 0}>
+                                        {/* {t("foodTable.firstPage")} */}
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-double-left" viewBox="0 0 16 16">
+                                            <path fill-rule="evenodd" d="M8.354 1.646a.5.5 0 0 1 0 .708L2.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0" />
+                                            <path fill-rule="evenodd" d="M12.354 1.646a.5.5 0 0 1 0 .708L6.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0" />
+                                        </svg>
+                                    </button>
+                                    <button
+                                        type="button"
+                                        className="btn btn-link p-0"
+                                        style={{ margin: '0.5rem', color: (page === 0) ? "grey" : "black" }}
+                                        onClick={() => setPage(page - 1)} disabled={page === 0}>
+                                        {/* {t("foodTable.previousPage")} */}
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
+                                            <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0" />
+                                        </svg>
+                                    </button>
+                                    <span style={{ paddingTop: "20px" }}> {page + 1} </span>
+                                    <button
+                                        type="button"
+                                        className="btn btn-link p-0"
+                                        style={{ margin: '0.5rem', color: (page + 1) * 10 >= vectorList.length ? "grey" : "black" }}
+                                        onClick={() => setPage(page + 1)} disabled={(page + 1) * 10 >= vectorList.length}>
+                                        {/* {t("foodTable.nextPage")} */}
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
+                                            <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708" />
+                                        </svg>
+                                    </button>
+                                    <button
+                                        type="button"
+                                        className="btn btn-link p-0"
+                                        style={{ color: (page + 1) * 10 >= vectorList.length ? "grey" : "black" }}
+                                        onClick={() => setPage(Math.ceil(vectorList.length / 10) - 1)} disabled={(page + 1) * 10 >= vectorList.length}>
+                                        {/* {t("foodTable.lastPage")} */}
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-double-right" viewBox="0 0 16 16">
+                                            <path fill-rule="evenodd" d="M3.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L9.293 8 3.646 2.354a.5.5 0 0 1 0-.708" />
+                                            <path fill-rule="evenodd" d="M7.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L13.293 8 7.646 2.354a.5.5 0 0 1 0-.708" />
+                                        </svg>
+                                    </button>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                </tfoot>
+
             </table>
             <button onClick={handleUserSelection} className="btn btn-secondary col-md-4 mt-3">{t("report.userSelectionButton")}</button>
         </div>
